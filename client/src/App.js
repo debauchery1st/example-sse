ca7e7b0493b76cca0d8fd003c841c2e6ee8b4511
Trevor Martin <trevor.j.martin@gmail.com>
2020-08-08 15:16:47 -0400

    chat title from env

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import StreamEvents from "./StreamEvents";
 
-function App({ title }) {
-  return <StreamEvents title={title || "Chat w/ HTML5 Server Sent Events"} />;
+function App() {
+  return (
+    <StreamEvents
+      title={process.env.REACT_APP_TITLE || "Chat w/ HTML5 Server Sent Events"}
+    />
+  );
 }
 
 export default App;
