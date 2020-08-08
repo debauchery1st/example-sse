2d9ba6862af2fd00969a867e0268c28bd883b334
Trevor Martin <trevor.j.martin@gmail.com>
2020-08-08 19:18:17 -0400

    fix word-wrap

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Message = (props) => {
-  return <pre>{`<${props.u || "system"}/> ${props.message}`}</pre>;
+  return <pre>{`<${props.u || "system"}> ${props.message}`}</pre>;
 };
 
 export default Message;
